6fa00bd58ae7fddfc6470426fceafd5285c33e6e
Jakub Wojciechowski <wojciechowskicuba@gmail.com>
2025-12-02 08:29:01 +0100

    adding naive metrics for benchmarking

---
+++ naive_metrics.py
@@ -0,0 +1,13 @@
+
+#https://www.researchgate.net/publication/309492895_Forecasting_to_Classification_Predicting_the_direction_of_stock_market_price_using_Xtreme_Gradient_Boosting
+### RSI = 100 - 100/(1+RS)
+### RS = avg gain over 14 days/avg loss over 14 days
+
+
+
+
+
+### %k = 100 * (C-L)/(H-L)
+### H = highest price over 14 days
+### L = lowesr price over 14 days
+### C = current price
